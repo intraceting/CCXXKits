32cd1e7ef077064e3ab686002e56037c361f7453
intraceting <traceting@gmail.com>
2025-08-31 19:40:31 +0800

    update

--- src/openssh/build.sh
+++ src/openssh/build.sh
@@ -48,10 +48,7 @@ PROJECT_NAME=${PROJECT_NAME^^}
 #
 if [ $(check_keyword ${BUILD_FLAGS} "rebuild-openssh") -eq 0 ];then
 {
-CHECK_LISTS[0]="${C2X2K_SYSROOT_PATH}/lib${C2X2K_TARGET_BITWIDE}/libopenssh.a"
-CHECK_LISTS[1]="${C2X2K_SYSROOT_PATH}/lib${C2X2K_TARGET_BITWIDE}/libopenssh.so"
-CHECK_LISTS[2]="${C2X2K_SYSROOT_PATH}/lib/libopenssh.a"
-CHECK_LISTS[3]="${C2X2K_SYSROOT_PATH}/lib/libopenssh.so"
+CHECK_LISTS[0]="${C2X2K_SYSROOT_PATH}/bin/sshd"
 }
 else
 {
