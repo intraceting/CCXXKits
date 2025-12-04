46229fe525bd07d8aac79018e07691bb8b925137
intraceting <traceting@gmail.com>
2025-12-04 22:17:21 +0800

    update

--- src/libsodium/build.sh
+++ src/libsodium/build.sh
@@ -92,29 +92,37 @@ tar --strip-components=1 -xvf "${SRC_FILE}" -C "${SRC_PATH}" >>${C2X2K_BUILD_LOG
 cd ${SRC_PATH}
 
 
+#
+if [ "${C2X2K_TARGET_PLATFORM}" == "aarch64" ] || [ "${C2X2K_TARGET_PLATFORM:0:5}" == "armv8" ];then
+    CONF_PARAMS="--host=${C2X2K_TARGET_MACHINE}"
+elif [ "${C2X2K_TARGET_PLATFORM}" == "arm" ] || [ "${C2X2K_TARGET_PLATFORM:0:5}" == "armv7" ];then
+    CONF_PARAMS="--host=${C2X2K_TARGET_MACHINE}"
+else
+    CONF_PARAMS="--host=${C2X2K_TARGET_MACHINE}"
+fi
+
+#
+./configure \
+    ${CONF_PARAMS} \
+    --prefix=${C2X2K_PREFIX_PATH} \
+    CC=${C2X2K_TARGET_COMPILER_C} \
+    CXX=${C2X2K_TARGET_COMPILER_CXX} \
+    CFLAGS="-fPIC" \
+    CXXFLAGS="-fPIC" \
+    >>${C2X2K_BUILD_LOG_FILE} 2>&1
+exit_if_error $? "Failed to configure ${PROJECT_NAME}." $?
+
+
 echo "#####################################################################################" >>${C2X2K_BUILD_LOG_FILE}
 
 #编译.
-make -j${C2X2K_BUILD_NPROC} \
-    CC=${C2X2K_TARGET_COMPILER_C} \
-    LD=${C2X2K_TARGET_COMPILER_LD} \
-    AR=${C2X2K_TARGET_COMPILER_AR} \
-    CFLAGS="-fPIC" \
-    VERBOSE=1 \
-    >>${C2X2K_BUILD_LOG_FILE} 2>&1 
+make -j${C2X2K_BUILD_NPROC}  >>${C2X2K_BUILD_LOG_FILE} 2>&1 
 exit_if_error $? "${PROJECT_NAME} build failed during compilation." $?
 
 echo "#####################################################################################" >>${C2X2K_BUILD_LOG_FILE}
 
 #安装.
-make install \
-    PREFIX=${C2X2K_PREFIX_PATH}/ \
-    CC=${C2X2K_TARGET_COMPILER_C} \
-    LD=${C2X2K_TARGET_COMPILER_LD} \
-    AR=${C2X2K_TARGET_COMPILER_AR} \
-    CFLAGS="-fPIC" \
-    VERBOSE=1 \
-    >>${C2X2K_BUILD_LOG_FILE} 2>&1 
+make install  >>${C2X2K_BUILD_LOG_FILE} 2>&1 
 exit_if_error $? "Failed to install ${PROJECT_NAME}." $?
 
 echo "#####################################################################################" >>${C2X2K_BUILD_LOG_FILE}
