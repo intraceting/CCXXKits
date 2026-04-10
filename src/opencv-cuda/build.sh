e6e0c248c2ce85b9460e5a80ab1128d6ca817322
zhanpengcheng <pengchengzhang@anson-tech.com>
2026-04-10 16:32:41 +0800

    update

--- src/opencv-cuda/build.sh
+++ src/opencv-cuda/build.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b0e8aaa63aaa1e18afa92f783e26ecac64f1af55f0f718691d2a2f9d57ff52a
-size 6543
+oid sha256:693efbd081d23d543340697912615cbe3b17085909b9b25f189714db5a76134f
+size 7014
